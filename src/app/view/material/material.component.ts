d4dbce16b126ad2fd68103730e30cab95f0578f4
Mario Alberto Morales <morales.mario@neoris.com>
2022-11-02 09:46:35 -0600

    only register cordenate x , y inside  the image area

--- src/app/view/material/material.component.ts
+++ src/app/view/material/material.component.ts
@@ -1,5 +1,8 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Refactory } from 'src/app/model/refactory.model';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-material',
@@ -13,33 +16,81 @@ export class MaterialComponent implements OnInit {
   @ViewChild("drag3") node3: ElementRef | undefined;
   @ViewChild("drag4") node4: ElementRef | undefined;
   @ViewChild("drag5") node5: ElementRef | undefined;
+  @ViewChild("imagen") imagen: ElementRef | undefined;
+  private currentScreenSize: string = '';
+  destroyed = new Subject<void>();
+ 
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'XSmall'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge'],
+  ]);
 
   public refractory: Refactory = this.InizializateRefractory();
   public refractorys: Refactory[] = [];
-  constructor() { }
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+    .observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ])
+    .pipe(takeUntil(this.destroyed))
+    .subscribe(result => {
+      for (const query of Object.keys(result.breakpoints)) {
+        if (result.breakpoints[query]) {
+          this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
+          console.log(this.currentScreenSize);
+          console.log(this.node1?.nativeElement);
+        }
+      }
+    });
+
+  }
 
   ngOnInit(): void {
   }
+
+  @HostListener('mouseenter') mouseHover(eventData: Event) {
+    console.log(eventData);
+    
+  }
+
   public GetValues() { 
     this.refractorys = [];
     for (let index = 0; index < this.Principal?.nativeElement.childNodes.length; index++) {
       const element = this.Principal?.nativeElement.childNodes[index].childNodes;
       element.forEach((elementChild:any) => {
-        console.log(elementChild.innerText)
-        console.log(elementChild.childNodes[0].getBoundingClientRect())
         this.SetRefractory(elementChild.childNodes[0].getBoundingClientRect(),elementChild.innerText);
       });
-      console.log(element);
     }
-    console.log(this.refractorys);
   }
 
   private SetRefractory(bunding: any, name: any) {
-    this.refractory = this.InizializateRefractory();
-    this.refractory.id = name;
-    this.refractory.configX = bunding.x;
-    this.refractory.configY = bunding.y;
-    this.refractorys.push(this.refractory);
+    let elem = document.elementFromPoint(bunding.x, bunding.y);
+    if (elem?.tagName === "IMG") {
+      this.refractory = this.InizializateRefractory();
+      this.refractory.id = name;
+      this.refractory.configX = bunding.x;
+      this.refractory.configY = bunding.y;
+      this.refractorys.push(this.refractory);
+    }
+  }
+
+  drop(event: any) {
+    console.log(event)
+  }
+
+  showCoords(event: any) {
+    var x = event.clientX;
+    var y = event.clientY;
+    console.log( "X: " + x + ", Y: " + y);
+    let elem = document.elementFromPoint(x, y);
+    console.log(elem)
   }
   
   private InizializateRefractory(): Refactory {
